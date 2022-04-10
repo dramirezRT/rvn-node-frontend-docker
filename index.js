32deaa21b063b3904d6950ddb54b8032df440975
Daniel Ramirez <louisdra8@protonmail.com>
2022-04-10 21:49:49 +0200

    Format for the reponse

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const PORT = 8080
 
 app.get('/', (req, res) => {
-    exec("cat /home/kingofthenorth/bootstrap-status.log && raven_status", (error, stdout, stderr) => {
+    exec("/usr/local/bin/raven_init node_status", (error, stdout, stderr) => {
     // exec("ls -lha && echo '' && ls /", (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
@@ -20,7 +20,11 @@ app.get('/', (req, res) => {
         out = stdout.split('\n').forEach( (element) => {
             result = result + "<div>" + element + "</div>"
         })
-        res.send(result)
+        if (result == "" ) {
+            res.send("<h1>Node is loading please try again later...</h1>")
+        } else{
+            res.send(result)
+        }
         // console.log(result);
     });
 })
